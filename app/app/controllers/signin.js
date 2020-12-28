60794ebea196b829230e73a1e5bfa628920b8806
Jeremie Ges <ges.jeremie@gmail.com>
2020-12-28 14:02:32 -0800

    feat(mvp): design not found page

--- app/app/controllers/signin.js
+++ app/app/controllers/signin.js
@@ -3,7 +3,6 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class SiginController extends Controller {
-  @service cookies;
   @service auth;
 
   @action
