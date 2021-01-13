1daf282096bd23ac3ccfda2e9b6fc14897432b4a
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-01-13 22:11:32 +0100

    feat: design menu and header

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,33 +1,32 @@
 import * as React from "react"
 
-// styles
-const pageStyles = {
-  // color: "#232129",
-  // padding: "96px",
-  // fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <div className="row">
-        <div className="col-xs-6">
-          BigSeat.
-        </div>
-        <div className="col-xs-6">
-          <div className="row">
-            <div className="col-xs-3">
-              Features
-            </div>
-            <div className="col-xs-3">
-              Pricing
-            </div>
-            <div className="col-xs-3">
-              About
-            </div>
-            <div className="col-xs-3">
-              Sign-in
+    <main>
+      <div className="wrapper">
+        <div className="row header">
+          <div className="col-xs-6">
+            <h1>
+              BigSeat.
+            </h1>
+          </div>
+          <div className="col-xs-6">
+            <div className="row menu">
+              <div className="col-xs-3">
+                Features
+              </div>
+              <div className="col-xs-3">
+                Pricing
+              </div>
+              <div className="col-xs-3">
+                About
+              </div>
+              <div className="col-xs-3">
+                <span className="menu__button">
+                  Sign-in
+                </span>
+              </div>
             </div>
           </div>
         </div>
